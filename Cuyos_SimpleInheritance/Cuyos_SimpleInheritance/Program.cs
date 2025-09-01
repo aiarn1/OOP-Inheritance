9c3c463a1ee9dfe27635fd7c90a6a8d5137699a0
Papellero <ACT-STUDENT@LAB3-PC28>
2025-09-01 14:30:48 +0800

    Hi

---
+++ Cuyos_SimpleInheritance/Cuyos_SimpleInheritance/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_SimpleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Louis Aian Cuyos
+            // IT306
+            // EXER_06: Simple Inheritance
+
+            // Create an instance of Student
+
+            Student student = new Student
+            {
+                Program =  "Bachelor of Science in Information Technology",
+                Name =     "Louis Aian Cuyos"
+            };
+            // Call Method to display student info
+            student.BasicInfo();
+
+            // Create an instance of RegularStudent
+            RegularStudent regularStudent = new RegularStudent
+            {
+                Program = "Bachelor of Science in Information Technology",
+                Name = "Sky Loon",
+                Section = "IT306",
+            };
+            // Call Method to display regular student info
+            regularStudent.BasicInfo();
+            regularStudent.SectionEnrolled();
+
+            // Create an instance of IrregularStudent
+            IrregularStudent irregularStudent = new IrregularStudent
+            {
+                Program = "Bachelor of Science in Information Technology",
+                Name = "John Doe",
+                UnitsEnrolled = 18
+            };
+            // Call Method to display irregular student info
+            irregularStudent.BasicInfo();
+            irregularStudent.EnrolledSemUnits();
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Cuyos_SimpleInheritance/Cuyos_SimpleInheritance/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_SimpleInheritance
+{
+     class Student // Base class
+    {
+        // Properties of the Student class
+        public string Program { get; set; }
+        public string Name { get; set; }
+
+        // Method to display student information
+        public void BasicInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;// Color the text to Cyan
+            Console.WriteLine("\n\t----------Student Information---------");
+            Console.WriteLine($"\n\tProgram :{Program}, \n\tName    : {Name}");
+        }
+
+    }
+         // RegularStudent inherits from Student
+    class RegularStudent : Student // Derived class
+    {
+        public string Section { get; set; }
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"\tSection: {Section}");
+        }
+    }
+         // IrregularStudent inherits from Student
+    class IrregularStudent : Student // Derived class
+    {
+        public int UnitsEnrolled { get; set; }
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine($"\tUnits Enrolled: {UnitsEnrolled}");
+        }
+    }
+}
