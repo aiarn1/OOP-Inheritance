249d6e12af7b4cd8beb69565b68803ffa85956ad
Papellero <ACT-STUDENT@LAB3-PC28>
2025-09-05 14:40:41 +0800

    Hi

---
+++ Cuyos_InheritanceWithConstructor/Cuyos_InheritanceWithConstructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_InheritanceWithConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Louis Aian Cuyos
+            // IT306
+            // EXER_07: Simple Inheritance with Constructor
+
+            // Create an instance of a Regular Student
+            RegularStudent regular = new RegularStudent("Sky", "BSIT", "IT306");
+            regular.SectionEnrolled();// Call Method to Display
+
+            // Create an instance of a Irregular Student
+            IrregularStudent irregular = new IrregularStudent("Jack", "BSIT", "18 Units");
+            irregular.EnrolledSemUnits();// Call Method to Display
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cuyos_InheritanceWithConstructor/Cuyos_InheritanceWithConstructor/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_InheritanceWithConstructor
+{
+     class Student
+    {
+        // Properties of the Student class
+        public string name;
+        public string program;
+
+        // Parameterized Constructor
+        public Student(string aname, string aprogram)
+        {
+            name = aname;
+            program = aprogram;
+        }
+
+        // Methods to Display
+        public void BasicInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;//Color the text to green
+            Console.WriteLine("\n\t-----Student Information-----");
+            Console.ForegroundColor = ConsoleColor.Cyan;//Color the text to Cyan
+            Console.WriteLine($"\n\tName    : {name},\n\tProgram : {program} ");
+        }
+    }
+    // Derived Class
+    class RegularStudent : Student
+    {
+        // Fields
+        public string section;
+
+        // Parameterized Constructor
+        public RegularStudent(string aname, string aprogram, string asection) : base(aname, aprogram)
+        {
+            section = asection;
+        }
+
+        // Methods to Display
+        public void SectionEnrolled()
+        {
+            BasicInfo();
+            Console.WriteLine("\tSection : " + section);
+        }
+
+    }
+    // Derived Class
+    class IrregularStudent : Student
+    {
+        // Fields
+        public string unitsEnrolled;
+
+        // Parameterized Constructor
+        public IrregularStudent(string aname, string aprogram, string aunitsEnrolled) : base(aname, aprogram)
+        {
+            unitsEnrolled = aunitsEnrolled;
+        }
+
+        // Methods to Display 
+        public void EnrolledSemUnits()
+        {
+            BasicInfo();
+            Console.WriteLine("\tEnrolled Units: " + unitsEnrolled);
+        }
+    }
+}
+
