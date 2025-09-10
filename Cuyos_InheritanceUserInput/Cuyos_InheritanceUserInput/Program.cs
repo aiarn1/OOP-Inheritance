19dfbd492d25bb7cd01c987b61a50d4923a929ac
Papellero <ACT-STUDENT@LAB3-PC28>
2025-09-10 14:49:04 +0800

    Hi

---
+++ Cuyos_InheritanceUserInput/Cuyos_InheritanceUserInput/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_InheritanceUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display the welcome message
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nSELECT THE TYPE OF SHIRT DO YOU WANT?");
+            Console.WriteLine("1. SLEEVELESS");
+            Console.WriteLine("2. POLOSHIRT");
+            Console.Write("Enter choice (1 or 2): ");
+            string choice = Console.ReadLine();
+
+            // Check the user's choice and create the appropriate shirt object
+            if (choice == "1")
+            {
+               
+                Console.Write("\nEnter the Brand of the Shirt  : ");
+                 string brand = Console.ReadLine();
+                Console.Write("Enter the Price of the Shirt  : ");
+                int price = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Quantity that you will buy: ");
+                string quantity = Console.ReadLine();
+                Console.Write("Enter the Color of the Shirt  : ");
+                string color = Console.ReadLine();
+
+                // Create a Sleeveless object with the provided details
+                Sleeveless sleeveless = new Sleeveless(brand, price, color, quantity);
+                sleeveless.MyDerived();
+            }
+            else if (choice == "2")
+            {
+               
+                Console.Write("\nEnter the Brand of the Shirt  : ");
+                string brand = Console.ReadLine();
+                Console.Write("Enter the Price of the Shirt  : ");
+                int price = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Neck Type of the Shirt: ");
+                string neckType = Console.ReadLine();
+                Console.Write("Enter the Size of the Shirt   : ");
+                string size =Console.ReadLine();
+
+                // Create a PoloShirt object with the provided details
+                PoloShirt poloShirt = new PoloShirt(brand, price, neckType, size);
+                poloShirt.MyDerived2();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
---
+++ Cuyos_InheritanceUserInput/Cuyos_InheritanceUserInput/Shirt.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_InheritanceUserInput
+{
+    class Shirt
+    {
+        //Fields
+
+        public string brand;
+        public int price;
+
+        // Parameterized Constructor
+        public Shirt(string aname, int aprice)
+        {
+            brand = aname;
+            price = aprice;
+        }
+        // Methods to Display
+        public void MyShirt()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n\tShirt Details:");
+            Console.WriteLine("\n\tBrand    : " + brand);
+            Console.WriteLine("\tPrice    : " + price);
+
+            
+        }
+    }
+
+    class Sleeveless : Shirt
+    {
+        // Fields
+        public string color;
+        public string quantity;
+
+        // Parameterized Constructor
+        public Sleeveless(string abrand, int aprice, string acolor, string aquantity) : base(abrand, aprice)
+        {
+            color = acolor;
+            quantity = aquantity;
+        }
+
+        // Methods to Display
+        public void MyDerived()
+        {
+            MyShirt();
+            Console.WriteLine("\tQuantity : " + quantity);           
+            Console.WriteLine("\tColor    : " + color);
+           
+        }
+
+    }
+    // Derived Class
+    class PoloShirt : Shirt
+    {
+        // Fields
+        public string neckType;
+        public string size;
+
+        // Parameterized Constructor
+        public PoloShirt(string abrand, int aprice, string aneckType, string asize) : base(abrand, aprice)
+        {
+            neckType = aneckType;
+            size = asize;
+        }
+
+        // Methods to Display 
+        public void MyDerived2()
+        {
+            MyShirt();
+            Console.WriteLine("\tNeck Type: " + neckType);
+            Console.WriteLine("\tSize     : " + size);
+            
+
+        }
+    }
+}
